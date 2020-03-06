4d57bda5ab44adf51055b9d56471d3dfa6daf722
Sunghyun Cho <mail@chosunghyun.com>
2020-03-06 16:39:20 +0900

    :zap: Speed Improvements Surprisingly removing all console.log did the job. Most likely due to the observer that logged every single activities.

--- core.js
+++ core.js
@@ -4,7 +4,7 @@ function containsUnicode(str, startUnicode, endUnicode) {
       return true;
     }
   }
-  return false
+  return false;
 }
 
 function removeNonKRComments() {
@@ -13,42 +13,26 @@ function removeNonKRComments() {
 
   for (var x = 0; x < commentList.length; x++) {
     comment = commentList[x].childNodes[1].childNodes[1].childNodes[3].childNodes[3].innerText;
-    if (containsUnicode(comment, "가", "힣")) {
-      console.log("한글인듯: "+ comment);
-    }
-    else {
-      console.log("영어인듯. 지움 ㅋㅋ: "+ comment);
+    if (!containsUnicode(comment, "가", "힣")) {
       commentList[x].parentNode.removeChild(commentList[x]);
       x--;
     }
   }
 }
 
-// Select the node that will be observed for mutations
 const target = document.evaluate('/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/div[1]/div/ytd-comments/ytd-item-section-renderer/div[3]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-
-// Options for the observer (which mutations to observe)
 const config = { attributes: true, childList: true, subtree: true };
 
-// Callback function to execute when mutations are observed
-const callback = function(mutationsList, observer) {
-    // Use traditional 'for loops' for IE 11
-    for(let mutation of mutationsList) {
-        if (mutation.type === 'childList') {
-            console.log('A child node has been added or removed.');
-            observer.disconnect();
-            removeNonKRComments();
-            console.log('And Stuffs got removed.');
-            observer.observe(target, config);
-        }
-        else if (mutation.type === 'attributes') {
-            console.log('The ' + mutation.attributeName + ' attribute was modified.');
-        }
+const callback = function (mutationsList, observer) {
+  for (let mutation of mutationsList) {
+    if (mutation.type === 'childList') {
+      observer.disconnect();
+      removeNonKRComments();
+      observer.observe(target, config);
     }
+  }
 };
 
-// Create an observer instance linked to the callback function
 const observer = new MutationObserver(callback);
-
-// Start observing the target node for configured mutations
 observer.observe(target, config);
+removeNonKRComments();
