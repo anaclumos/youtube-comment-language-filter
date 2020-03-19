aa4d932e205e7461647d0276661ccc6612574bd7
Sunghyun Cho <mail@chosunghyun.com>
2020-03-19 13:17:13 +0900

    :zap: Retries until HTMLs are found & Better Performance & Better Code
    * Waits and retries if HTML elements are not found
    * No more hang when loading new comments (problem: observer.disconnect)
    * Code structure improvements

--- scripts/content.js
+++ scripts/content.js
@@ -1,97 +1,82 @@
-var buttonPressed = false;
-const config = { attributes: false, childList: true, subtree: true };
-const callback = function (mutationsList, observer) {
-  for (let mutation of mutationsList) {
-    if (mutation.type === 'childList') {
-      hideComments();
-    }
-  }
-};
-const observer = new MutationObserver(callback);
-
-
-window.onload = () => {
-  var CLFHeader = document.createElement('h2');
-  CLFHeader.id = "CLFHeader";
-  CLFHeader.textContent = "언어 필터";
-
-  var CLFButton = document.createElement('button');
-  CLFButton.id = "CLFButton";
-  CLFButton.textContent = "한글";
-
-  var CLFFooter = document.createElement('h2');
-  CLFFooter.id = "CLFFooter";
-  CLFFooter.textContent = "스크롤해서 댓글을 로딩하세요";
-
-  var meta = document.getElementById('meta');
-  var primary = document.getElementById('primary');
-
-  meta.append(CLFHeader);
-  meta.append(CLFButton);
-  primary.append(CLFFooter);
-  CLFButton.style
-  CLFButton.addEventListener('click', () => {
-    if (buttonPressed) {
-      showComments();
-      CLFButton.textContent = "한글";
-      buttonPressed = false;
-    }
-    else {
-      filterComments();
-      CLFButton.textContent = "전체 언어";
-      buttonPressed = true;
-    }
-  });
+async function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function containsUnicode(str, startUnicode, endUnicode) {
-  for (var i = 0; i < str.length; i++) {
-    if (startUnicode.charCodeAt(0) <= str.charCodeAt(i) && str.charCodeAt(i) <= endUnicode.charCodeAt(0)) {
+async function wait(ms) {
+  await sleep(ms);
+}
+
+function containsHangul(string) {
+  var stlen = string.length;
+  var i = 0;
+  for (i = 0; i < stlen; i++) {
+    if (44032 <= string.charCodeAt(i) && string.charCodeAt(i) <= 55203) {
       return true;
     }
   }
   return false;
 }
 
-function hideComments() {
-  observer.disconnect();
+function onlyShowHangulComments() {
   var commentList = document.getElementsByTagName("ytd-comment-thread-renderer");
-  var comment;
-  for (var x = 0; x < commentList.length; x++) {
-    if (commentList[x].id == "nonKR-comment" && commentList[x].style == "display: none") {
-      continue;
-    }
-    else if (commentList[x].id == "nonKR-comment" && commentList[x].style != "display: none") {
-      commentList[x].style = "display: none";
-      continue;
-    }
-    else {
-      comment = commentList[x].childNodes[1].childNodes[1].childNodes[3].childNodes[3].childNodes[1].innerText;
-      if (!containsUnicode(comment, "가", "힣")) {
-        commentList[x].style = "display: none";
-        commentList[x].id = "nonKR-comment";
-      }
-      else {
-        commentList[x].id = "korean-comment";
+  for (var comment of commentList) {
+    if (comment.id === "") {
+      var commentString = comment.childNodes[1].childNodes[1].childNodes[3].childNodes[3].childNodes[1].innerText;
+      if (containsHangul(commentString)) {
+        comment.id = "contains-hangul";
+      } else {
+        comment.id = "no-hangul";
       }
     }
-  }
-  const target = document.evaluate('/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/div[1]/div/ytd-comments/ytd-item-section-renderer/div[3]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-  observer.observe(target, config);
-}
-
-function showComments() {
-  observer.disconnect();
-  var commentList = document.getElementsByTagName("ytd-comment-thread-renderer");
-  for (var x = 0; x < commentList.length; x++) {
-    if (commentList[x].id == "nonKR-comment") {
-      commentList[x].style = "";
+    if (comment.id === "no-hangul") {
+      comment.style = "display: none";
+      console.log("hided one comment.");
     }
   }
 }
 
-function filterComments() {
-  const target = document.evaluate('/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/div[1]/div/ytd-comments/ytd-item-section-renderer/div[3]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-  observer.observe(target, config);
-  hideComments();
+var observer = new MutationObserver((mutationList) => {
+  onlyShowHangulComments();
+});
+
+window.onload = () => {
+  console.log("onload");
+  var CLFButton = document.createElement('button');
+  CLFButton.id = "CLFButton";
+  CLFButton.style =
+    `position: relative; box-sizing: border-box; border: none; border-radius: 2px; padding: 10px 16px; min-width: 64px;
+   vertical-align: middle; text-align: center; font-family: Roboto, Arial, sans-serif; font-size: 14px;
+   font-weight: 500; outline: none; cursor: pointer; background-color: transparent; color: rgb(133, 133, 133);`;
+  var CLFFooter = document.createElement('h2');
+  CLFFooter.id = "CLFFooter";
+  CLFFooter.style = "text-align: center; color: rgb(133, 133, 133); margin-bottom: 100px;";
+
+  var meta = document.getElementById("meta");;
+  var primary = document.getElementById("primary");;
+
+  while (meta === undefined) {
+    wait(2000);
+    meta = document.getElementById("meta");
+  }
+
+  while (primary === undefined) {
+    wait(2000);
+    primary = document.getElementById("primary");
+  }
+
+  console.log("meta is " + meta);
+  console.log("primary is " + primary);
+
+  meta.append(CLFButton);
+  primary.append(CLFFooter);
+
+  CLFButton.textContent = "한글을 포함한 댓글만 보기";
+  CLFFooter.textContent = "한글을 포함한 댓글만 보는 중입니다.";
+
+  CLFButton.addEventListener('click', () => {
+    onlyShowHangulComments();
+    const config = { attributes: false, childList: true, subtree: true };
+    const target = document.evaluate('/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/div[1]/div/ytd-comments/ytd-item-section-renderer/div[3]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    observer.observe(target, config);
+  });
 }
