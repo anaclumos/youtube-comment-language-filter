1c37ebc732e979312316585e1d35feadb0c2387c
Sunghyun Cho <mail@chosunghyun.com>
2020-03-22 19:34:32 +0900

    :sparkles: Open "Learn More" on install

--- extension/scripts/background.js
+++ extension/scripts/background.js
@@ -1,9 +1,15 @@
+chrome.runtime.onInstalled.addListener(function (object) {
+  chrome.tabs.create({ url: "https://chosunghyun.com/youtube-comment-language-filter" }, function (tab) {
+    //console.log("");
+  });
+});
+
 chrome.tabs.onUpdated.addListener(
-  function(tabId, changeInfo, tab) {
+  function (tabId, changeInfo, tab) {
     // read changeInfo data and do something with it
     // like send the new url to contentscripts.js
     if (changeInfo.url) {
-      chrome.tabs.sendMessage( tabId, {
+      chrome.tabs.sendMessage(tabId, {
         message: 'page moved!',
         url: changeInfo.url
       })
