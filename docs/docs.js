02de73f9f3779dfcb1d0eb0761ae79d052816476
Sunghyun Cho <mail@chosunghyun.com>
2020-03-22 23:02:04 +0900

    :pencil: Docs change

--- docs/docs.js
+++ docs/docs.js
@@ -30,7 +30,7 @@ function setLangtoKorean() {
   <ol>
       <li>확장 프로그램 아이콘을 클릭하세요.</li>
       <li>Settings를 누르세요.</li>
-      <li>보고 싶은 댓글 언어들을 선택하고 Save를 눌러 저장하세요.</li>
+      <li>필요 없는 옵션들을 선택하고 Save를 눌러 저장하세요.</li>
       <li>YouTube 동영상 페이지 설명란 하단에 댓글을 필터링할 수 있는 언어 선택 메뉴가 생길 것입니다.</li>
       <li>문의 사항은 <a
               href="mailto:mail@chosunghyun.com">mail@chosunghyun.com</a>로 메일 부탁드립니다.</li>
