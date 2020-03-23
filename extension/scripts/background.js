3b2a4825f1e8d03402a7212d19d493cbf463fae4
Sunghyun Cho <mail@chosunghyun.com>
2020-03-23 15:40:56 +0900

    :heart: UX updates

--- extension/scripts/background.js
+++ extension/scripts/background.js
@@ -1,13 +1,11 @@
-chrome.runtime.onInstalled.addListener(function (object) {
-  chrome.tabs.create({ url: "https://chosunghyun.com/youtube-comment-language-filter" }, function (tab) {
-    //console.log("");
-  });
-});
+// chrome.runtime.onInstalled.addListener(function (object) {
+//   chrome.tabs.create({ url: "https://chosunghyun.com/youtube-comment-language-filter" }, function (tab) {
+//     //console.log("");
+//   });
+// });
 
 chrome.tabs.onUpdated.addListener(
-  function (tabId, changeInfo, tab) {
-    // read changeInfo data and do something with it
-    // like send the new url to contentscripts.js
+  function (tabId, changeInfo) {
     if (changeInfo.url) {
       chrome.tabs.sendMessage(tabId, {
         message: 'page moved!',
